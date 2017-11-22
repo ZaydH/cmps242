81bc6d54426060553deb8d2ca10240ad3a0b17f4
Benjamin Sherman <bcsherma@ucsc.edu>
2017-11-21 23:03:05 -0800

    the RNN is in place. No FF or softmax yet.

--- project/src/network.py
+++ project/src/network.py
@@ -29,7 +29,38 @@ def construct_network(vocab_size):
     # create the embedding lookup
     embedded = tf.nn.embedding_lookup(embed_matrix, X)
 
-    # 
+    # create RNN cell
+    lstm_cell = tf.contrib.rnn.LSTMCell(const.RNN_HIDDEN_SIZE, state_is_tuple = True)
+
+    print(lstm_cell, embedded)
+
+    # get rnn outputs
+    rnn_output, rnn_state = tf.nn.dynamic_rnn(lstm_cell, embedded, dtype=tf.float32)
+
+    print(rnn_output)
+
+    # transpose rnn_output into a time major form
+    rnn_output = tf.transpose(rnn_output, [1, 0, 2])
+    
+    # get the output of the last time-step
+    rnn_final_output = tf.gather(
+            rnn_output, const.WINDOW_SIZE - 1
+        )
+
+    return {
+            'X': X, 'Y': Y, 'RNN_OUTPUT': rnn_final_output
+        }
+
+def run_network(feature_dict, sequences, targets):
+    """
+    run a tensor flow session and try feeding the network stuff.
+    just for testing right now
+    """
+    
+    # start the session
+    init_op = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init_op)
 
 # main
 if __name__ == '__main__':
@@ -41,4 +72,7 @@ if __name__ == '__main__':
     sequences, targets = data_parser.get_examples(input_str, 1000)
 
     # 
-    construct_network(vocab_size)
+    network_features = construct_network(vocab_size)
+
+    # 
+    run_network(network_features, sequences, targets)
